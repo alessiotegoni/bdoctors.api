de311827bb5e3e34c466c1d859704046f96d6284
alessiotegoni <110291544+alessiotegoni@users.noreply.github.com>
2025-01-20 18:21:36 +0100

    Update and rename DoctorController.js to doctorControllers.js

--- Controllers/doctorControllers.js
+++ Controllers/doctorControllers.js
@@ -3,8 +3,8 @@ const connection = require('../data/db');
 function index(req, res) {
     const sql = `SELECT * FROM doctors`;
     connection.query(sql, (err, doctors) => {
-        if (err) return res.status(404).json({ error: `error` });
-        res.json({ doctors })
+        if (err) throw new Error(err)
+        res.json(doctors)
     })
 }
 module.exports = index
