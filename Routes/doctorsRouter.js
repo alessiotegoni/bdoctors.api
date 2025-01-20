4f4d24b4cedb0eea65d915cc809bef5632b42989
alessiotegoni <110291544+alessiotegoni@users.noreply.github.com>
2025-01-20 18:22:56 +0100

    Update and rename doctorsRouter.js to doctorRoutes.js

--- Routes/doctorsRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const index = require('../Controllers/DoctorController');
-
-router.get('/',index);
-
-module.exports = router;
