1a00818e988d6eb43de2494355852a0a90ec049e
antoniorusso2 <russoantonio303@outlook.it>
2025-01-20 16:16:59 +0100

    env variables for connection

--- data/db.js
+++ data/db.js
@@ -1,13 +1,14 @@
-const sql = require('mysql2');
+const sql = require('mysql2')
 
- const connection = sql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'boolean123@',
-    database: 'bdoctors.sql'
+const connection = sql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 })
-connection.connect((err)=>{
-    if(err) throw err
+connection.connect((err) => {
+  if (err) throw err
+  console.log('Connected to database')
 })
 
-module.exports = connection;
+module.exports = connection
