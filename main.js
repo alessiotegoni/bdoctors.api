4b6edabbc133a419b88497de0a9fdedfb2502fb7
antoniorusso2 <russoantonio303@outlook.it>
2025-01-20 17:18:34 +0100

    rimosso il file env, aggiunto configurazione in package json per utilizzo variabili d'ambiente e aggiunto un sample per run applicazione in locale

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //express base config
 const express = require('express')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT
 
 //middlewares
 const errorsHandler = require('./middlewares/errorsHandler')
