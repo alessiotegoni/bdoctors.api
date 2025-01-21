1193770fe4b82c1aaa30e0b1e7266b54a7c154f8
tega <alessiotegoni4@gmail.com>
2025-01-21 11:27:19 +0100

    fix: specializations controller

--- Controllers/doctorControllers.js
+++ Controllers/doctorControllers.js
@@ -29,7 +29,9 @@ function getDoctorsSpecializations(_, res) {
   connection.query("SELECT * FROM specializations", (err, specializations) => {
     if (err) throw new Error(err.message);
 
-    res.status(200).json(specializations);
+    res
+      .status(200)
+      .json(specializations.map((s) => ({ value: s.id, label: s.name })));
   });
 }
 
