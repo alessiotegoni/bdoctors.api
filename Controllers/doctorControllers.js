4f0815b217b81b8461aed61770e8c7698f4731a4
tega <alessiotegoni4@gmail.com>
2025-01-21 16:01:36 +0100

    add: store doctor, store review controllers (antoniorusso2)

--- Controllers/doctorControllers.js
+++ Controllers/doctorControllers.js
@@ -35,4 +35,118 @@ function getDoctorsSpecializations(_, res) {
   });
 }
 
-module.exports = { index, show, getDoctorsSpecializations };
+function storeDoctor(req, res) {
+  //   console.log(req.body)
+  const { firstName, lastName, email, phone, address, specializationsIds } =
+    req.body;
+
+  if (
+    !firstName ||
+    firstName.length > 50 ||
+    !lastName ||
+    lastName.length > 50 ||
+    typeof firstName !== "string" ||
+    !address ||
+    !specializationsIds
+  ) {
+    return res.status(400).json({
+      error: "Missing required fields",
+      message:
+        "First name, last name, specializations and address are required",
+    });
+  }
+
+  //aggiunta nuovo dottore al database
+  //cerco in database se la mail inserita risulta già registrata
+  const mailSql = `SELECT * FROM doctors WHERE email = ?`;
+
+  connection.query(mailSql, [email], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    if (results.length > 0) {
+      return res.status(400).json({ message: "email already registered" });
+    }
+
+    //se la mail non esiste allora si procede alla creazione di un nuovo dottore
+    const sql = `INSERT INTO doctors (first_name, last_name, email, phone, address) VALUES (?, ?, ?, ?, ?)`;
+
+    connection.query(
+      sql,
+      [firstName, lastName, email, phone, address],
+      (err, results) => {
+        if (err) {
+          return res.status(500).json({ message: err.message });
+        }
+
+        const sql2 = `INSERT INTO doctor_specializations (doctor_id, specialization_id) VALUES ?`;
+
+        const newId = results.insertId;
+
+        //array di coppia [id Dottore - id specializzazione] che saranno eseguiti in query
+        //utilizzando questo metodo con una sola query è possibile inserire piú righe nella tabella ponte
+        const values = specializationsIds.map((specialization) => [
+          newId,
+          specialization,
+        ]);
+
+        //aggiunta nella tabella ponte id dottore creato ed id specializzazioni
+        connection.query(sql2, [values], (err, _) => {
+          if (err) return res.status(500).json({ message: err.message });
+          return res.status(201).json({
+            message: "doctor created successfully",
+            newId: newId,
+          });
+        });
+      }
+    );
+  });
+}
+
+function storeReview(req, res) {
+  //id del medico
+  const doctorId = parseInt(req.params.id);
+
+  //recupero parametri dalla body request
+  const { firstName, lastName, rating, text } = req.body;
+
+  //campi nome e voto necessari
+  if (
+    !firstName ||
+    firstName.length > 50 ||
+    typeof firstName !== "string" ||
+    rating < 1 ||
+    rating > 5
+  ) {
+    return res.status(400).json({
+      error: "Missing required fields",
+      message: "name and vote are required",
+    });
+  }
+
+  //query
+  const sql = `INSERT INTO reviews (first_name, last_name, review_text, rating, doctor_id) VALUES (?,?,?,?,?)`;
+
+  connection.query(
+    sql,
+    [firstName.trim(), lastName.trim(), text.trim(), rating, doctorId],
+    (err, results) => {
+      if (err) return res.status(500).json({ message: err.message });
+
+      // console.log(results);
+
+      res.status(201).json({
+        message: "review created successfully",
+        newId: results.insertId,
+      });
+    }
+  );
+}
+
+module.exports = {
+  index,
+  show,
+  getDoctorsSpecializations,
+  storeDoctor,
+  storeReview,
+};
--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const { index, show, getDoctorsSpecializations } = require("../controllers/doctorControllers");
+const {
+  index,
+  show,
+  getDoctorsSpecializations,
+  storeDoctor,
+  storeReview,
+} = require("../controllers/doctorControllers");
 
 const router = express.Router();
 
-router.get("/specializations", getDoctorsSpecializations)
+router.get("/specializations", getDoctorsSpecializations);
 
 router.get("/", index);
 router.get("/:id", show);
 
+router.post("/", storeDoctor);
+router.post("/reviews/:id", storeReview);
 
 module.exports = router;
