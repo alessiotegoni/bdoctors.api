aad927c9b925cdb9d96ed819138b94366affdf6e
antoniorusso2 <russoantonio303@outlook.it>
2025-01-23 02:02:14 +0100

    completed basic middleware validation logic

--- middlewares/validateInput.js
+++ middlewares/validateInput.js
@@ -2,12 +2,12 @@
 //creare un oggetto rules con le regole di ogni input da controllare
 
 const inputValues = {
-  firstName: 'Pi',
-  lastName: 'P',
-  email: 'ab@.c',
-  phone: '1234+567890',
+  firstName: 'Pipp',
+  lastName: 'Pipp',
+  email: 'abc@.',
+  phone: '1234567890',
   address: 'Via Roma, 1',
-}
+};
 
 const rules = {
   firstName: {
@@ -21,7 +21,7 @@ const rules = {
   email: {
     min: 3,
     max: 50,
-    specialCharacters: ['@', '.'],
+    isRequired: ['@', '.'],
   },
   phone: {
     min: 3,
@@ -31,54 +31,69 @@ const rules = {
     specialCharacters: ['+'],
   },
   address: {
-    min: 3,
-    max: 50,
+    min: 5,
+    max: 255,
     startsWith: ['Via', 'Piazza'],
   },
-}
+};
 
 function validateInput(input = {}, rules = {}) {
-  console.log('start validate')
+  console.log('start validate');
 
   //funzione per la validazione
-  const errors = []
+  const errors = [];
 
   //controllo rispetto valori di lunghezza minimi e massimi
   for (const key in input) {
+    // console.log(`lunghezza ${key}: ${input[key].length}`)
+
     if (input[key].length > rules[key].max || input[key].length < rules[key].min) {
-      errors.push(`${key} must be between ${rules[key].min} and ${rules[key].max} characters`)
+      errors.push(
+        `la lunghezza di ${key} deve essere compresa tra ${rules[key].min} e ${rules[key].max} caratteri`
+      );
     }
-  }
 
-  //controllo rispetto caratteri iniziali
-  for (const key in input) {
-    //controlla se la chiave corrisponde all'indirizzo	allora controlla se l'indirizzo inizia con Via o piazza
-    if (rules[key] === 'address' && rules[key].startsWith) {
+    //controllo mail
+    if (key === 'email') {
+      //ciclo for per il controllo di ogni carattere
+      //controlla per ogni valore contenuto nell'oggetto rules, se i caratteri indicati alla chiave 'isRequired' sono presenti all'interno della chiave dell'oggetto input
+      for (let i = 0; i < rules[key].isRequired.length; i++) {
+        if (!input[key].includes(rules[key].isRequired[i])) {
+          errors.push(`${key} deve contenere ${rules[key].isRequired[i]}`);
+          break;
+        }
+      }
+    }
+
+    //controllo indirizzo
+    if (key === 'address') {
+      //split per controllare il la prima parola dell'indirizzo
+      const address = input[key].split(' ');
       //ciclo for per il controllo di ogni carattere
       for (let i = 0; i < rules[key].startsWith.length; i++) {
-        if (
-          input[key] !== rules.address.startsWith[0] &&
-          input[key] !== rules.address.startsWith[1]
-        ) {
+        if (address[0] !== rules[key].startsWith[0] && address[0] !== rules[key].startsWith[1]) {
           errors.push(
-            `${key} must start with ${rules[key].startsWith[0]} or ${rules[key].startsWith[1]}`
-          )
+            `l'indirizzo deve iniziare con ${rules[key].startsWith[0]} o ${rules[key].startsWith[1]}`
+          );
+          break;
         }
       }
     }
 
-    //? controllo sul numero di telefono da sistemare
-
-    if (rules[key] === 'phone' && rules[key].startsWith) {
+    //controllo phone
+    if (key === 'phone') {
       //ciclo for per il controllo di ogni carattere
-      for (let i = 0; i < rules[key].startsWith.length; i++) {
-        if (input[key][i] === rules[key].startsWith[0]) {
-          errors.push(`il simbolo + è permesso solo all'inizio`)
+      for (let i = 0; i < input[key].length; i++) {
+        //controllo che nel numero non sia presente il simbolo + ma che possa esserlo solo all'inizio
+        if (input[key].includes('+') && input[key].indexOf('+') !== 0) {
+          errors.push(`il simbolo + è permesso solo all'inizio`);
+          break;
         }
       }
     }
   }
-  return errors
+
+  return errors;
 }
 
-console.log(validateInput(inputValues, rules))
+console.log(validateInput(inputValues, rules));
