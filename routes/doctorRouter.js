7a216ff1aa058d63e6ad0ee25a8259ec60704abd
antoniorusso2 <russoantonio303@outlook.it>
2025-01-23 16:46:07 +0100

    fix specialization route

--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const {
   index,
   show,
@@ -6,17 +6,18 @@ const {
   storeDoctor,
   storeReview,
   getFilteredDoctors,
-} = require("../controllers/doctorControllers");
+} = require('../controllers/doctorControllers');
 
 const router = express.Router();
 
-router.get("/", index);
-router.get("/:id", show);
+router.get('/', index);
+router.get('/specializations', getDoctorsSpecializations);
 
-router.get("/specializations", getDoctorsSpecializations);
-router.get("/:first_name?/:last_name?/:specializations?", getFilteredDoctors);
+router.get('/:id', show);
+router.get('/:first_name?/:last_name?/:specializations?', getFilteredDoctors);
 
-router.post("/", storeDoctor);
-router.post("/:id/review", storeReview);
+router.post('/', storeDoctor);
+
+router.post('/:id/review', storeReview);
 
 module.exports = router;
