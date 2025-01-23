04b04620e4ce78aaddbf56ea10b8648c82fa8c34
antoniorusso2 <russoantonio303@outlook.it>
2025-01-23 17:25:26 +0100

    add simple filter with name and email

--- Controllers/doctorControllers.js
+++ Controllers/doctorControllers.js
@@ -1,15 +1,13 @@
-const connection = require("../data/db");
+const connection = require('../data/db');
 
 function index(req, res) {
-
-  const { doctor, specializations, min_rating } = req.query
+  // const { firstName } = req.query;
 
   // doctor is first_name, last_name concatenati or email
   // specializations is an array of string with specializationsId
   // min rating is a string
 
-
-  const sql = `SELECT
+  let sql = `SELECT
     doctors.*,
     GROUP_CONCAT(DISTINCT specializations.name ORDER BY specializations.name SEPARATOR ', ') AS specializations,
     AVG(reviews.rating) AS avg_rating
@@ -22,8 +20,27 @@ LEFT JOIN reviews
     ON doctors.id = reviews.doctor_id
 GROUP BY doctors.id`;
 
+  const filter = req.query.filter;
+  console.log(filter);
+
+  if (filter) {
+    sql = `SELECT * FROM doctors WHERE first_name OR email LIKE ?;`;
+
+    console.log(filter);
+
+    connection.query(sql, `%${[filter]}%`, (err, doctors) => {
+      // console.log(err);
+      console.log(doctors);
+      if (err) {
+        return res.status(500).json({ error: 'error' });
+      }
+      res.json(doctors);
+    });
+    return;
+  }
+
   connection.query(sql, (err, doctors) => {
-    if (err) res.status(500).json({ err: "error" });
+    if (err) res.status(500).json({ err: 'error' });
     res.json(doctors);
   });
 }
@@ -34,31 +51,22 @@ function getFilteredDoctors(req, res) {
   // Retrieve parameters from URL path using req.query
   const { first_name, last_name, specialization } = req.query;
 
-  const sql = `SELECT
-    doctors.*,
-    GROUP_CONCAT(DISTINCT specializations.name ORDER BY specializations.name SEPARATOR ', ') AS specializations,
-    AVG(reviews.rating) AS rating
-FROM doctors
-JOIN doctor_specializations
-    ON doctors.id = doctor_specializations.doctor_id
-JOIN specializations
-    ON doctor_specializations.specialization_id = specializations.id
-LEFT JOIN reviews
-    ON doctors.id = reviews.doctor_id
-GROUP BY doctors.id`;
+  const sql = `SELECT * FROM doctors WHERE first_name or email LIKE ?;`;
 
-  const values = [
-    first_name || null,
-    first_name ? `%${first_name}%` : null,
-    last_name || null,
-    last_name ? `%${last_name}%` : null,
-    specialization || null,
-    specialization ? `%${specialization}%` : null,
-  ];
+  // const values = [
+  //   first_name || null,
+  //   first_name ? `%${first_name}%` : null,
+  //   last_name || null,
+  //   last_name ? `%${last_name}%` : null,
+  //   specialization || null,
+  //   specialization ? `%${specialization}%` : null,
+  // ];
 
-  connection.query(sql, values, (err, doctors) => {
+  const value = req.query.firstName || req.query.email;
+
+  connection.query(sql, value, (err, doctors) => {
     if (err) {
-      return res.status(500).json({ error: "error" });
+      return res.status(500).json({ error: 'error' });
     }
     res.json(doctors);
   });
@@ -67,7 +75,7 @@ GROUP BY doctors.id`;
 function show(req, res) {
   const id = parseInt(req.params.id);
   if (isNaN(id)) {
-    return res.status(400).json({ error: "id not found" });
+    return res.status(400).json({ error: 'id not found' });
   }
   const IdSql = `SELECT
                   doctors.*,
@@ -81,41 +89,42 @@ function show(req, res) {
                   GROUP BY doctors.id`;
   connection.query(IdSql, [id], (err, [doctor]) => {
     if (err) {
-      return res.status(500).json({ error: "server error" });
+      return res.status(500).json({ error: 'server error' });
     }
     if (doctor.length === 0) {
-      return res.status(404).json({ error: "Not found" });
+      return res.status(404).json({ error: 'Not found' });
     }
     res.status(200).json(doctor);
   });
 }
 
 function getDoctorsSpecializations(_, res) {
-  connection.query("SELECT * FROM specializations", (err, specializations) => {
+  connection.query('SELECT * FROM specializations', (err, specializations) => {
     if (err) throw new Error(err.message);
-    res
-      .status(200)
-      .json(specializations.map((s) => ({ value: s.id, label: s.name })));
+    res.status(200).json(specializations.map((s) => ({ value: s.id, label: s.name })));
   });
 }
 
 function storeDoctor(req, res) {
-  const { firstName, lastName, email, phone, address, specializationsIds } =
-    req.body;
+  const { firstName, lastName, email, phone, address, specializationsIds } = req.body;
 
   if (
     !firstName ||
     firstName.length > 50 ||
+    firstName.length < 3 ||
     !lastName ||
     lastName.length > 50 ||
-    typeof firstName !== "string" ||
+    lastName.length < 3 ||
+    typeof firstName !== 'string' ||
+    typeof lastName !== 'string' ||
     !address ||
-    !specializationsIds
+    !specializationsIds ||
+    !email.includes('@') ||
+    !email.includes('.')
   ) {
     return res.status(400).json({
-      error: "Missing required fields",
-      message:
-        "First name, last name, specializations and address are required",
+      error: 'Missing required fields',
+      message: 'First name, last name, specializations and address are required',
     });
   }
 
@@ -128,44 +137,39 @@ function storeDoctor(req, res) {
       return res.status(500).json({ message: err.message });
     }
     if (results.length > 0) {
-      return res.status(400).json({ message: "email already registered" });
+      return res.status(400).json({ message: 'email already registered' });
     }
 
     //se la mail non esiste allora si procede alla creazione di un nuovo dottore
     const sql = `INSERT INTO doctors (first_name, last_name, email, phone, address) VALUES (?, ?, ?, ?, ?)`;
 
-    connection.query(
-      sql,
-      [firstName, lastName, email, phone, address],
-      (err, results) => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-
-        const sql2 = `INSERT INTO doctor_specializations (doctor_id, specialization_id) VALUES ?`;
-
-        const newId = results.insertId;
-
-        //array di coppia [id Dottore - id specializzazione] che saranno eseguiti in query
-        //utilizzando questo metodo con una sola query è possibile inserire piú righe nella tabella ponte
-        const values = specializationsIds.map((specialization) => [
-          newId,
-          specialization,
-        ]);
-
-        //aggiunta nella tabella ponte id dottore creato ed id specializzazioni
-        connection.query(sql2, [values], (err, _) => {
-          if (err) return res.status(500).json({ message: err.message });
-          return res.status(201).json(newId);
-        });
+    connection.query(sql, [firstName, lastName, email, phone, address], (err, results) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
       }
-    );
+
+      const sql2 = `INSERT INTO doctor_specializations (doctor_id, specialization_id) VALUES ?`;
+
+      const newId = results.insertId;
+
+      //array di coppia [id Dottore - id specializzazione] che saranno eseguiti in query
+      //utilizzando questo metodo con una sola query è possibile inserire piú righe nella tabella ponte
+      const values = specializationsIds.map((specialization) => [newId, specialization]);
+
+      //aggiunta nella tabella ponte id dottore creato ed id specializzazioni
+      connection.query(sql2, [values], (err, _) => {
+        if (err) return res.status(500).json({ message: err.message });
+        return res.status(201).json(newId);
+      });
+    });
   });
 }
 
 function storeReview(req, res) {
   //id del medico
   const doctorId = parseInt(req.params.id);
+
+  console.log(doctorId);
 
   //recupero parametri dalla body request
   const { firstName, lastName, rating, reviewText } = req.body;
@@ -174,13 +178,13 @@ function storeReview(req, res) {
   if (
     !firstName ||
     firstName.length > 50 ||
-    typeof firstName !== "string" ||
+    typeof firstName !== 'string' ||
     rating < 1 ||
     rating > 5
   ) {
     return res.status(400).json({
-      error: "Missing required fields",
-      message: "name and vote are required",
+      error: 'Missing required fields',
+      message: 'name and vote are required',
     });
   }
 
@@ -189,7 +193,7 @@ function storeReview(req, res) {
 
   connection.query(
     sql,
-    [firstName.trim(), lastName.trim(), reviewText.trim(), rating, doctorId],
+    [firstName.trim(), lastName.trim(), reviewText && reviewText.trim(), rating, doctorId],
     (err, results) => {
       if (err) return res.status(500).json({ message: err.message });
 
--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -14,10 +14,9 @@ router.get('/', index);
 router.get('/specializations', getDoctorsSpecializations);
 
 router.get('/:id', show);
-router.get('/:first_name?/:last_name?/:specializations?', getFilteredDoctors);
+router.post('/:id/review', storeReview);
+// router.get('/:first_name?/:last_name?/:specializations?', getFilteredDoctors);
 
 router.post('/', storeDoctor);
-
-router.post('/:id/review', storeReview);
 
 module.exports = router;
