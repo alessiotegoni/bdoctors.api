e302c7d360d530d732465dde92a7720e048fd22d
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-28 10:33:49 +0100

    FIX require

--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const { index, show, storeDoctor, storeReview } = require('../controllers/doctorControllers');
+const express = require("express");
+const {
+  index,
+  show,
+  storeDoctor,
+  storeReview,
+} = require("../Controllers/doctorControllers");
 
-const validateInput = require('../middlewares/validateInput');
+const validateInput = require("../middlewares/validateInput");
 
 const router = express.Router();
 
-router.get('/', index);
-router.get('/:id', show);
+router.get("/", index);
+router.get("/:id", show);
 
-router.post('/', validateInput, storeDoctor);
-router.post('/:id/review', storeReview);
+router.post("/", validateInput, storeDoctor);
+router.post("/:id/review", storeReview);
 
 module.exports = router;
